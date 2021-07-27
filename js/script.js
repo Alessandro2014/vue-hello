457a862786e8081a8b384d8fd7c66e6621f5dfc0
Alessandro2014 <alessandromerenda22@gmail.com>
2021-07-27 16:15:48 +0200

    inserimento data e stampa

---
+++ js/script.js
@@ -0,0 +1,15 @@
+console.log('vue ys ready', Vue);
+
+const root = new Vue({
+    el: '#root',
+    data: {
+        tDocumento: 'Carta di identità',
+        cIdentita: 'NH6737',
+        nome: 'Pinco',
+        cognome: 'Pallino',
+        residenza: 'via contarini, 9',
+        citta: 'sperduta',
+        foto: '',
+        myClass: 'font-size',
+    } 
+});
