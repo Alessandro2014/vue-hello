638484cefc8fc9e0a552ed697233c7117947583c
Alessandro2014 <alessandromerenda22@gmail.com>
2021-07-27 17:16:36 +0200

    aggiunta immagine

--- js/script.js
+++ js/script.js
@@ -3,13 +3,13 @@ console.log('vue ys ready', Vue);
 const root = new Vue({
     el: '#root',
     data: {
+        image: 'https://tgscloud.it/storage/default/user-default.png',
         tDocumento: 'Carta di identità',
         cIdentita: 'NH6737',
         nome: 'Pinco',
         cognome: 'Pallino',
         residenza: 'via contarini, 9',
         citta: 'sperduta',
-        foto: '',
         myClass: 'font-size',
     } 
 });
